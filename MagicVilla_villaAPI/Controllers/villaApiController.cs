4a6555a21601183dd758b8acaaa36cd9b17fff94
maiallam57 <maimallam57@gmail.com>
2024-08-15 01:57:11 +0300

    getVilla

---
+++ MagicVilla_villaAPI/Controllers/villaApiController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_villaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_villaAPI.Controllers
+{
+    [Route("/api/villaApi")]
+    [ApiController]
+    public class villaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<villa> GetVillas()
+        {
+            return new List<villa>()
+            {
+                new villa{Id=1, Name ="pool villa"},
+                new villa{Id=2, Name ="beach villa"}
+
+            };
+        }
+    }
+}
---
+++ MagicVilla_villaAPI/Models/villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_villaAPI.Models
+{
+    public class villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
